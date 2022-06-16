db30efcc8eb0cb52c3353ce627c24da3e4bcd515
Muhammad Aswan <muhaswan190897@gmail.com>
2022-06-16 22:59:20 +0800

    update migration and foreign key

--- database/migrations/2022_06_16_144944_add_foreign_keys_to_transaction_table.php
+++ database/migrations/2022_06_16_144944_add_foreign_keys_to_transaction_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('transaction', function (Blueprint $table) {
-            $table->dropForeign('appoinment_id');
+            $table->dropForeign('appointment_id');
         });
     }
 };
