cc6cbf8e2e5cb6cd4ecc010b3476b5473426e3eb
Muhammad Aswan <muhaswan190897@gmail.com>
2022-08-31 20:28:09 +0800

    update request role

--- app/Http/Requests/Role/StoreRoleRequest.php
+++ app/Http/Requests/Role/StoreRoleRequest.php
@@ -30,7 +30,7 @@ class StoreRoleRequest extends FormRequest
     {
         return [
             'title' => [
-                'required', 'string', 'max:255',
+                'required', 'string', 'max:255', 'unique:role',
             ],
         ];
     }
